1b554c24eb8711036fd0026a2761f4263a86a491
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2021-11-01 18:33:13 +0200

    Update GameBoard.cpp

--- pacman_project/Project1/GameBoard.cpp
+++ pacman_project/Project1/GameBoard.cpp
@@ -1,9 +1,26 @@
 #include "GameBoard.h"
 
+void GameBoard::initBoard()
+{
+    int rowInd, colInd;
+
+    for (rowInd = 0; rowInd < boardSize; rowInd++)
+        board[rowInd][0] = border;
+    for (colInd = 0; colInd < boardSize; colInd++)
+        board[0][colInd] = border;
+    for (rowInd = 0; rowInd < boardSize; rowInd++)
+        board[rowInd][boardSize - 1] = border;
+    for (colInd = 0 ; colInd < boardSize ; colInd++)
+        board[boardSize - 1][colInd] = border;
+    for (rowInd = 1; rowInd < boardSize - 1; rowInd++)
+        for (colInd = 1; colInd < boardSize - 1; colInd++)
+            board[rowInd][colInd] = space;
+}
+
 void GameBoard::printBorders()
 {
     //gotoxy(1,1);
-    for (int i = startingRow ; i <= endingRow ; i++)
+    for (int i = 0 ; i < 30 ; i++)
         cout << border ;
     for (int i = startingRow ; i <= endingRow ; i++)
         cout << border ;
@@ -11,5 +28,5 @@ void GameBoard::printBorders()
         cout << border ;
     for (int i = startingRow ; i <= endingRow ; i++)
         cout << border ;
-    //check if it works
+    
 }
