78a64a4100e295fe807ecb8280c40901959674ea
chenberger <88037210+chenberger@users.noreply.github.com>
2021-11-13 16:55:49 +0200

    changes by chen

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+#include "ThePacmanGame.h"
+#include "GameBoard.h" //should be to ThePacmanGame.h
+
+int main()
+{
+	ThePacmanGame game;
+	game.initGame();
+	game.runGame();
+
+	return 0;
+}
