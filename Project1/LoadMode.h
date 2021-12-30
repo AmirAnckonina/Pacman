7695a8afca734f63a78751aa6be3f85cb6ac1fe2
chenberger <88037210+chenberger@users.noreply.github.com>
2021-12-31 00:58:29 +0200

    coiiman

--- Project1/LoadMode.h
+++ Project1/LoadMode.h
@@ -1,8 +1,28 @@
 #pragma once
 
 #include "SilentMode.h"
+using std::vector;
+using std::ifstream;
+using std::cin;
+using std::cin;
 
 class LoadMode : public SilentMode
 {
+	int currStepsFile = 0;
+	int currResultFile = 0;
+	ifstream stepsFile;
+	ifstream resultFile;
+	vector <string> stepsfilesArr;
+	vector <string> resultfilesArr;
 
+	Direction direction;
+public:
+	void loadAllStepsAndResultFiles();
+	void openFilesForRead();
+	void closeCurrFiles();
+	void readInfoFromStepsFile();
+	void convertInputToDirection(string _dir);
+	//setPacDirectionFromFile();
+	//setGhostsDirectionFromFile();
+	//setFruitDirectionFromFile();
 };
